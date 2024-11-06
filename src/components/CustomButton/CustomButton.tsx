b26f827fa180db133790b14644304d6693e74f4c
dhanur350 <dhanur297@gmail.com>
2024-11-06 09:47:22 +0530

    homepage design

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -3,6 +3,7 @@ import './CustomButton.scss';
 
 interface Props {
   baseClassname?: string;
+  round?: boolean;
   greenTheme?: boolean;
   greenShade2?: boolean;
   children?: any;
@@ -10,10 +11,11 @@ interface Props {
 
 function CustomButton(props: Props) {
   
-  const { baseClassname, greenShade2, children } = props;
+  const { baseClassname, round, greenShade2, children } = props;
 
   return (
     <button className={classNames(["custom-button", baseClassname], {
+      "custom-button-round-shape" : round,
       "custom-button-green-shade-2" : greenShade2
     })}>{children}</button>
   )
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { CustomButton } from "@/components";
+import awardImg from "@/assets/award-image.png";
 import "./Home.scss";
 
 function Home() {
@@ -12,15 +13,16 @@ function Home() {
             <h1 className="homepage-welcome-intro-paradise-text">Paradise</h1>
             <h1 className="homepage-welcome-intro-world-kangra">World Kangra</h1>
           </div>
-          <span>Your Ultimate Beauty Destination</span> 
-          <span>in Himachal Pradesh!</span>
-          <p>Nestled in the serene town of Kangra, Himachal Pradesh, Paradise World is more than just a beauty salon.</p>
+          <span className="homepage-your-ultimate-beauty">Your Ultimate Beauty Destination</span> 
+          <span className="homepage-your-ultimate-beauty">in Himachal Pradesh!</span>
+          <p className="homepage-nestled-in-the-serene">Nestled in the serene town of Kangra, Himachal Pradesh, Paradise World is more than just a beauty salon.</p>
           <span>it's a retreat where beauty meets tranquility.</span>
-          <CustomButton greenShade2>Explore more</CustomButton>
+          <CustomButton round baseClassname="homepage-explore-more-button" greenShade2>Explore more</CustomButton>
         </div>
 
         <div className="homepage-welcome-image-container">
-          
+          <img src={awardImg} alt="award-image" className="homepage-welcome-award-image" />
+          <div className="homepage-welcome-image-background"></div>
         </div>
 
       </div>
