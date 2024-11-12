d0d20cb10abcf54d4b32e71cd5ac65e09a6e4c71
dhanur350 <dhanur297@gmail.com>
2024-11-12 19:15:54 +0530

    store redux connect

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import paradiseReducer from "./slice";
 import rootSaga from "./saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
-
+    paradiseapp: paradiseReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([sagaMiddleware])
 })
---
+++ src/store/slice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const paradiseSlice = createSlice({
+  name: "paradiseapp",
+  initialState: {
+  },
+  reducers: {
+  }
+});
+
+export const {} = paradiseSlice.actions;
+
+export default paradiseSlice.reducer;
