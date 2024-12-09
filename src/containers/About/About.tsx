698becb1af6817d4f9ab6130ff073bd8ffb7b64b
dhanur350 <dhanur297@gmail.com>
2024-12-09 22:27:00 +0530

    paradise

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -2,8 +2,12 @@ import "./About.scss";
 
 function About() {
   return (
-    <div>
-      
+    <div className="about-page-container">
+      <div className="about-page-banner-container">
+        <h1 className="about-page-title lora-font">About page</h1>
+      </div>
+
+      div.about-page-
     </div>
   )
 }
